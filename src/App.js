0da0f911ebafd74388f81f879ba84823d0e623d8
Group14 <roballa@siue.edu>
2026-02-03 14:29:59 -0600

    Switched from using postcss to using vite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import './out.css';
 
 
 export default function App() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
+
